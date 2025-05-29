8d5e49f487ee01e132a624c79dbb2d29066abd01
iivertt <ingrid88cruz@gmail.com>
2025-05-29 12:59:13 -0700

    Create Calculator app.js

---
+++ Calculator app.js
@@ -0,0 +1,36 @@
+let input = document.getElementById('inputBox'); 
+let buttons = document.querySelectorAll('button'); 
+
+let string = ""; 
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+  button.addEventListener('click', (e) => { 
+    let buttonText = e.target.innerHTML; 
+    
+    if(buttonText === '='){ 
+      try {
+        string = eval(string); 
+        input.value = string; 
+      } catch (error) {
+        input.value = 'Error'; 
+        string = ''; 
+      }
+    }
+    
+    else if(buttonText === 'AC'){ 
+      string = '';
+      input.value = string;
+    }
+    
+    else if(buttonText === 'DEL'){ 
+      string = string.slice(0, -1); 
+      input.value = string;
+    }
+    
+    else{ 
+      string += buttonText; 
+      input.value = string;
+    }
+  })
+})
